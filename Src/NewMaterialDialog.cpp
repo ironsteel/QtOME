983a715003c21d7f774e1fb503fa3400dbd59f55
Rangel Ivanov <Iron_steel_88@abv.bg>
2011-04-18 19:28:38 +0300

    Don't show create button
    Don't show create button in dialog if
    no material name is given

--- Src/NewMaterialDialog.cpp
+++ Src/NewMaterialDialog.cpp
@@ -1,14 +1,39 @@
 #include "NewMaterialDialog.h"
 #include "ui_NewMaterialDialog.h"
+#include "MaterialManager.hpp"
 
 NewMaterialDialog::NewMaterialDialog(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::NewMaterialDialog)
 {
     ui->setupUi(this);
+    ui->createButton->setEnabled(false);
+}
+
+void NewMaterialDialog::processDialog()
+{
+
+    QString tempName = ui->materialName->text();
+    if(!tempName.isEmpty()) {
+        this->materialName = tempName;
+    }
 }
 
 NewMaterialDialog::~NewMaterialDialog()
 {
     delete ui;
+}
+
+void NewMaterialDialog::accept()
+{
+    QString workDir = MaterialManager::getSingletonPtr()->getWorkDir();
+
+}
+
+void NewMaterialDialog::activateOkButton(QString text)
+{
+    if(text.isEmpty())
+        ui->createButton->setEnabled(false);
+    else
+        ui->createButton->setEnabled(true);
 }
--- Src/NewMaterialDialog.h
+++ Src/NewMaterialDialog.h
@@ -13,10 +13,18 @@ class NewMaterialDialog : public QDialog
 
 public:
     explicit NewMaterialDialog(QWidget *parent = 0);
+    void processDialog();
+
     ~NewMaterialDialog();
+public slots:
+    void accept();
+    void activateOkButton(QString text);
 
 private:
     Ui::NewMaterialDialog *ui;
+    QString materialName;
+    QString vertexShaderName;
+    QString fragmentShaderName;
 };
 
 #endif // NEWMATERIALDIALOG_H
